088d9642b4e4774c906dfaba8c9c5a082d497561
Muhammad Fahad Mustafa <mfahadm8@gmail.com>
2023-08-08 20:37:26 +0500

    removed unnessary ssm params' '

--- src/network_stack/network_stack.py
+++ src/network_stack/network_stack.py
@@ -20,7 +20,6 @@ class NetworkStack(Stack):
         vpcConstruct = Vpc(self, "Vpc", config)
         self._vpc = vpcConstruct.vpc
         self.__push_vpc_id_cidr()
-        self.__push_subnets_route_tables_ids()
 
     def __push_vpc_id_cidr(self):
         vpc_id = self._vpc.vpc_id
@@ -41,65 +40,3 @@ class NetworkStack(Stack):
             string_value=vpc_cidr_block,
             parameter_name=self.config["ssm_infra"] + "vpcCidrBlock",
         )
-
-    def __push_subnets_route_tables_ids(self):
-        if self.config["network"]["vpc"]["create_natgateway"] == 1:
-            private_subnets = self._vpc.select_subnets(
-                subnet_type=ec2.SubnetType.PRIVATE_WITH_EGRESS
-            ).subnets
-
-            for index, subnet in enumerate(private_subnets):
-                ssm.StringParameter(
-                    scope=self,
-                    id=f"privateSubnet{index+1}",
-                    tier=ssm.ParameterTier.STANDARD,
-                    string_value=subnet.subnet_id,
-                    parameter_name=self.config["ssm_infra"] + f"privateSubnet{index+1}",
-                )
-
-                ssm.StringParameter(
-                    scope=self,
-                    id=f"privateRouteTable{index+1}",
-                    tier=ssm.ParameterTier.STANDARD,
-                    string_value=subnet.route_table.route_table_id,
-                    parameter_name=self.config["ssm_infra"]
-                    + f"privateRouteTable{index+1}",
-                )
-
-                ssm.StringParameter(
-                    scope=self,
-                    id=f"privateSubnetAz{index+1}",
-                    tier=ssm.ParameterTier.STANDARD,
-                    string_value=subnet.availability_zone,
-                    parameter_name=self.config["ssm_infra"]
-                    + f"privateSubnetAz{index+1}",
-                )
-
-        public_subnets = self._vpc.select_subnets(
-            subnet_type=ec2.SubnetType.PUBLIC
-        ).subnets
-
-        for index, subnet in enumerate(public_subnets):
-            ssm.StringParameter(
-                scope=self,
-                id=f"publicSubnet{index+1}",
-                tier=ssm.ParameterTier.STANDARD,
-                string_value=subnet.subnet_id,
-                parameter_name=self.config["ssm_infra"] + f"publicSubnet{index+1}",
-            )
-
-            ssm.StringParameter(
-                scope=self,
-                id=f"publicRouteTable{index+1}",
-                tier=ssm.ParameterTier.STANDARD,
-                string_value=subnet.route_table.route_table_id,
-                parameter_name=self.config["ssm_infra"] + f"publicRouteTable{index+1}",
-            )
-
-            ssm.StringParameter(
-                scope=self,
-                id=f"publicSubnetAz{index+1}",
-                tier=ssm.ParameterTier.STANDARD,
-                string_value=subnet.availability_zone,
-                parameter_name=self.config["ssm_infra"] + f"publicSubnetAz{index+1}",
-            )
